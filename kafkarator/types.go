12886392d93af8097729eb94fc68e960b771effd
Ellen Yu <ellen.yu@computas.com>
2026-01-09 14:35:41 +0100

    Message to writer (#11)
    * :recycle: add message as param to write
    
    * :white_check_mark: pass linter

--- kafkarator/types.go
+++ kafkarator/types.go
@@ -8,23 +8,23 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 )
 
-// Message is a message that has been read off of a topic. It is more or less identical to the struct that is
-// implemented by the underlying kafka library. We choose to expose our own type in order to insulate the consumer from
+// Message is a message that can either be written to topic or be read off of a topic. It is more or less identical to the struct that is
+// implemented by the underlying kafka library. We choose to expose our own type in order to insulate the writer/consumer from
 // such implementation details.
 type Message struct {
-	// Topic indicates which topic this message was consumed from.
+	// Topic indicates which topic this message is written to/consumed from.
 	Topic string
 
-	// Partition is the partition of the topic that the message came from.
+	// Partition is the partition of the topic that the message came from. Only relevant for consuming
 	Partition int
 
-	// Offset is the offset, or "address", of the message on the partition.
+	// Offset is the offset, or "address", of the message on the partition. Only relevant for consuming
 	Offset int64
 
 	// Key is the key of the message.
 	Key []byte
 
-	// Value is the actual payload of the message. This is what you want to unmarshal!
+	// Value is the actual payload of the message. This is what you want to unmarshal when consuming
 	Value []byte
 
 	Decoded any
--- kafkarator/writer.go
+++ kafkarator/writer.go
@@ -53,9 +53,13 @@ func (w *Writer) Close(ctx context.Context) error {
 // Write writes the given message with headers to the topic. An important side effect is
 // that if there is an OpenTelemetry tracing span associated with the context, it is extracted
 // and included in the header that is sent to Kafka.
-func (w *Writer) Write(ctx context.Context, key, value []byte, headers map[string][]byte) error {
+func (w *Writer) Write(ctx context.Context, message *Message) error {
 	ctx, span := w.tel.Tracer().Start(ctx, "kafkarator.Writer.Write")
 	defer span.End()
+
+	if message == nil {
+		return fmt.Errorf("message cannot be nil")
+	}
 
 	if w.closed {
 		span.RecordError(fmt.Errorf("writer is closed"))
@@ -64,7 +68,7 @@ func (w *Writer) Write(ctx context.Context, key, value []byte, headers map[strin
 
 	span.SetAttributes(attribute.String("writer.topic", w.topic))
 
-	traceHeaders := injectTraceContext(ctx, headers)
+	traceHeaders := injectTraceContext(ctx, message.Headers)
 
 	kafkaHeaders := make([]kafka.Header, 0, len(traceHeaders))
 	for k, v := range traceHeaders {
@@ -76,8 +80,8 @@ func (w *Writer) Write(ctx context.Context, key, value []byte, headers map[strin
 			Topic:     &w.topic,
 			Partition: kafka.PartitionAny,
 		},
-		Value:   value,
-		Key:     key,
+		Value:   message.Value,
+		Key:     message.Key,
 		Headers: kafkaHeaders,
 	}
 
