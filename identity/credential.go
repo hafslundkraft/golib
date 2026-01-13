400cfce1c1bc8ea10a914b2c9319087cc87bcc7a
Eigil Skjæveland <eigil.skjaveland@hafslund.no>
2026-01-13 16:01:27 +0100

    identity: Add audience option to TokenSource

--- identity/credential.go
+++ identity/credential.go
@@ -8,5 +8,25 @@ import (
 
 // A Credential is anything that can be used to produce an oauth2.TokenSource
 type Credential interface {
-	TokenSource(ctx context.Context, scopes ...string) oauth2.TokenSource
+	TokenSource(ctx context.Context, opts ...func(*TokenOptions)) oauth2.TokenSource
+}
+
+// TokenOptions defines options for configuring the token requests.
+type TokenOptions struct {
+	scopes   []string
+	resource string
+}
+
+// WithResource sets the target resource for the token.
+func WithResource(resource string) func(*TokenOptions) {
+	return func(o *TokenOptions) {
+		o.resource = resource
+	}
+}
+
+// WithScopes specifies scopes to request in the token.
+func WithScopes(scopes ...string) func(*TokenOptions) {
+	return func(o *TokenOptions) {
+		o.scopes = scopes
+	}
 }
